f03b748342975ad0ca39ccb2e3b7e03069bf859d
Minichev Alexandr <139385189+panasonique@users.noreply.github.com>
2026-02-24 18:54:19 +0100

    Delete 20_Variables_Demo {demo_var}/20_Custom_Step.py

--- 20_Variables_Demo {demo_var}/20_Custom_Step.py
+++
@@ -1,9 +0,0 @@
-info = {
-    "name": "Smart Float",
-    "type": "input_custom",
-    "prop_id": "demo_var",
-    "default": 0.5,
-    "min": 0.0, "max": 10.0,
-    "description": "Округляется до целого только если > 1.0"
-}
-# Логика скрипта может использовать props.demo_var
