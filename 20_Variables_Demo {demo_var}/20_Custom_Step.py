07ccb8e297db0ac46363301a3b5180681a67587e
Minichev Alexandr <139385189+panasonique@users.noreply.github.com>
2026-02-24 18:41:20 +0100

    Add files via upload

---
+++ 20_Variables_Demo {demo_var}/20_Custom_Step.py
@@ -0,0 +1,9 @@
+info = {
+    "name": "Smart Float",
+    "type": "input_custom",
+    "prop_id": "demo_var",
+    "default": 0.5,
+    "min": 0.0, "max": 10.0,
+    "description": "Округляется до целого только если > 1.0"
+}
+# Логика скрипта может использовать props.demo_var
